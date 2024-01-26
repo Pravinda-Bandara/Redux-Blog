023f6a86723fc60e210e10bd458a2629342b05e4
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-01-27 03:43:07 +0530

    implement addPostForm and add to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import {PostsList} from "./features/post/PostsList.jsx";
+import AddPostForm from "./features/post/AddPostForm.jsx";
 
 function App() {
   return (
     <>
+        <AddPostForm/>
       <PostsList/>
     </>
   )
