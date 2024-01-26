26b4e9c341210ad23b323ee1db3372080f5ef5f1
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-01-27 03:42:44 +0530

    postAdded function implement in postSlice

--- src/features/post/PostsList.jsx
+++ src/features/post/PostsList.jsx
@@ -1,5 +1,4 @@
 import {useSelector} from "react-redux";
-import {isTSIntersectionType} from "eslint-plugin-react/lib/util/ast.js";
 import {selectAllPosts} from "./postSlice.js";
 
 export function PostsList() {
