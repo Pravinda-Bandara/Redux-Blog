097e527c911188a784d2907b6b0bea8733568fa4
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-02-01 02:01:26 +0530

    add header

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return (
+        <header className="header">
+            <h1>Redux Blog</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Post</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
