533c574d2eb9ee301b606bc05b6dbd0c1f3cf460
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-01-31 20:51:16 +0530

    Update userSlice ,fetch users

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,15 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    { id: '0', name: 'pravinda bandara' },
-    { id: '1', name: 'kasun kalhara' },
-    { id: '2', name: 'gimhan sampath' }
-]
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users;
